2e8ba9926289f452362ef348e6eb2d9779c9cc5e
Eugene <115879896+eugenezabrodski@users.noreply.github.com>
2023-02-19 11:51:12 +0300

    19.02.23

---
+++ DataFlowSwiftUI/DataFlowSwiftUI/DataFlowSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataFlowSwiftUIApp.swift
+//  DataFlowSwiftUI
+//
+//  Created by Eugene on 19/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct DataFlowSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
